4a00a0a01b98edd13cc475d39a7d50bd8925442a
claude cavour <aristide22lapo@gmail.com>
2021-06-30 06:54:02 +0200

    init project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { ShopComponent } from './components/shop/shop.component';
 import { SingleProductComponent } from './components/shop/single-product/single-product.component';
 import { AddProductComponent } from './components/shop/add-product/add-product.component';
 import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
-import { CarComponent } from './components/shop/car/car.component';
+
 import { HeaderComponent } from './components/partial/header/header.component';
 import { FooterComponent } from './components/partial/footer/footer.component';
 import { NotFoundComponent } from './components/partial/not-found/not-found.component';
@@ -17,6 +18,7 @@ import { HeaderPageComponent } from './components/partial/header-page/header-pag
 import { QuickViewModalComponent } from './components/partial/modal/quick-view-modal/quick-view-modal.component';
 import { AddToCartModalComponent } from './components/partial/modal/add-to-cart-modal/add-to-cart-modal.component';
 import { DeleteProductModalComponent } from './components/partial/modal/delete-product-modal/delete-product-modal.component';
+import { CartComponent } from './components/shop/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -27,18 +29,19 @@ import { DeleteProductModalComponent } from './components/partial/modal/delete-p
     SingleProductComponent,
     AddProductComponent,
     EditProductComponent,
-    CarComponent,
     HeaderComponent,
     FooterComponent,
     NotFoundComponent,
     HeaderPageComponent,
     QuickViewModalComponent,
     AddToCartModalComponent,
-    DeleteProductModalComponent
+    DeleteProductModalComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
